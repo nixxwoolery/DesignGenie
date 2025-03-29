16daeb5ce13cb5fc383dddd356cea32b7918a114
nixxwoolery <nicolettewoolery@hotmail.com>
2025-03-29 11:32:42 -0500

    navbar update

--- js/main.js
+++ js/main.js
@@ -404,6 +404,15 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
 // document.addEventListener("DOMContentLoaded", () => {
 //     // ==== INITIALIZATION ====
 //     let currentStep = 1;
